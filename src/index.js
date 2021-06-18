f2fe05ff2c8f6ca05f416b84f58be2e684633dda
Mohammad Za'areer <zaa92reer@gmail.com>
2021-06-18 22:35:55 +0300

    lab-26 done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import './design/index.scss';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
